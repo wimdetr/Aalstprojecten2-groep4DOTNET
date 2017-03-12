1139f12bc194fe4c85fb8eadb07e0436b8b1f4f7
~dreeki~ <~dreeki~@DESKTOP-G68EGDU>
2017-03-12 16:18:02 +0100

    Feature 6

---
+++ src/Aalstprojecten2-groep4DOTNET/Models/NogViewModels/WachtwoordVergetenViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aalstprojecten2_groep4DOTNET.Models.NogViewModels
+{
+    public class WachtwoordVergetenViewModel
+    {
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
