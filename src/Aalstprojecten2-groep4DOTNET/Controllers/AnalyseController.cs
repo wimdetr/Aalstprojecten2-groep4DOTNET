e2981b9c713e9539df8998c64a1d9e68a05757ae
niels <niels@NIELSPC>
2017-03-29 14:48:28 +0200

    merge

---
+++ src/Aalstprojecten2-groep4DOTNET/Controllers/AnalyseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Aalstprojecten2_groep4DOTNET.Controllers
+{
+    public class AnalyseController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult AnalyseBekijken()
+        {
+            return View();
+        }
+
+        public IActionResult AnalyseOverzicht()
+        {
+            return View();
+        }
+
+        public IActionResult AnalyseBaat1()
+        {
+            return View();
+        }
+
+        public IActionResult AnalyseKost()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Aalstprojecten2-groep4DOTNET/wwwroot/js/tableScript.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('.addBtn').on('click', function () {
+        //var trID;
+        //trID = $(this).closest('tr'); // table row ID 
+        addTableRow();
+    });
+    $('.addBtnRemove').click(function () {
+        $(this).closest('tr').remove();
+    })
+    var i = 1;
+    function addTableRow() {
+        var tempTr = $('<tr><td><input type="number" id="Uren" class="form-control" placeholder="€" /></td><td><input type="number" id="BrutoLoon" class="form-control" placeholder="€"/></td><td><input type="number" id="TotaleLoonKost" class="form-control" placeholder="€"/></td></tr>').on('click', function () {
+        });
+        $("#tableAddRow").append(tempTr)
+        i++;
+    }
+});
+
+$(document).ready(function () {
+    $('.addBtn2').on('click', function () {
+        //var trID;
+        //trID = $(this).closest('tr'); // table row ID 
+        addTableRow();
+    });
+    $('.addBtnRemove').click(function () {
+        $(this).closest('tr').remove();
+    })
+    var i = 1;
+    function addTableRow() {
+        var tempTr = $('<tr><td><input type="number" id="Uren" class="form-control" placeholder="€" /></td><td><input type="number" id="BrutoLoon" class="form-control" placeholder="€"/></td><td><input type="number" id="TotaleLoonKost" class="form-control" placeholder="€"/></td></tr>').on('click', function () {
+        });
+        $("#tableAddRow2").append(tempTr)
+        i++;
+    }
+});
