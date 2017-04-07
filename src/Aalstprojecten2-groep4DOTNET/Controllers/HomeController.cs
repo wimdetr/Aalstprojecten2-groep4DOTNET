b844a9280774df698c09e4a8ad6548c128194b7b
dreeki <andreas.dewitte@hotmail.com>
2017-04-07 20:57:53 +0200

    profiel aanpassen + wachtwoord veranderen, probleem voor op te vangen als info verandert is, en dan naar wachtwoord veranderen gaan, omdat model niet doorgegeven kan worden naar get methode vanuit view

--- src/Aalstprojecten2-groep4DOTNET/Controllers/HomeController.cs
+++ src/Aalstprojecten2-groep4DOTNET/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Aalstprojecten2_groep4DOTNET.Models;
+using Aalstprojecten2_groep4DOTNET.Models.AccountViewModels;
 using Aalstprojecten2_groep4DOTNET.Models.Domein;
 using Aalstprojecten2_groep4DOTNET.Models.ViewModels.Home;
 using Microsoft.AspNetCore.Identity;
@@ -35,6 +36,157 @@ namespace Aalstprojecten2_groep4DOTNET.Controllers
             return View(new TeTonenAnalysesViewModel(analyses));
         }
 
-        
+        public IActionResult ProfielAanpassen()
+        {
+            JobCoach jc = _jobCoachRepository.GetByEmail(User.Identity.Name);
+            ProfielAanpassenViewModel origineelModel = new ProfielAanpassenViewModel()
+            {
+                Bus = jc.BusBedrijf,
+                Email = jc.Email,
+                Gemeente = jc.GemeenteBedrijf,
+                Naam = jc.Naam,
+                NaamBedrijf = jc.NaamBedrijf,
+                Nummer = jc.NummerBedrijf,
+                Postcode = jc.PostcodeBedrijf,
+                Straat = jc.StraatBedrijf,
+                Voornaam = jc.Voornaam
+            };
+            ProfielAanpassenViewModel model = new ProfielAanpassenViewModel(origineelModel);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ProfielAanpassen(ProfielAanpassenViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    JobCoach jc = _jobCoachRepository.GetByEmail(User.Identity.Name);
+                    jc.BusBedrijf = model.Bus;
+                    jc.GemeenteBedrijf = model.Gemeente;
+                    jc.Naam = model.Naam;
+                    jc.NaamBedrijf = model.NaamBedrijf;
+                    jc.NummerBedrijf = model.Nummer;
+                    jc.PostcodeBedrijf = model.Postcode;
+                    jc.StraatBedrijf = model.Straat;
+                    jc.Voornaam = model.Voornaam;
+                    _jobCoachRepository.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ProfielAanpassenDoorgaanZonderOpslaan(ProfielAanpassenViewModel model)
+        {
+            if (ControleerOfModelVerandertIs(model))
+            {
+                return View(model);
+            }
+            return RedirectToAction(nameof(WachtwoordAanpassen));
+        }
+
+        public IActionResult DoorgaanMetOpslaan(ProfielAanpassenViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    JobCoach jc = _jobCoachRepository.GetByEmail(User.Identity.Name);
+                    jc.BusBedrijf = model.Bus;
+                    jc.GemeenteBedrijf = model.Gemeente;
+                    jc.Naam = model.Naam;
+                    jc.NaamBedrijf = model.NaamBedrijf;
+                    jc.NummerBedrijf = model.Nummer;
+                    jc.PostcodeBedrijf = model.Postcode;
+                    jc.StraatBedrijf = model.Straat;
+                    jc.Voornaam = model.Voornaam;
+                    _jobCoachRepository.SaveChanges();
+                    return RedirectToAction(nameof(WachtwoordAanpassen));
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+            return RedirectToAction(nameof(ProfielAanpassenDoorgaanMetOpslaanNietMogelijk), model);
+        }
+
+        public IActionResult ProfielAanpassenDoorgaanMetOpslaanNietMogelijk(ProfielAanpassenViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult DoorgaanZonderOpslaan(ProfielAanpassenViewModel model)
+        {
+            return RedirectToAction(nameof(WachtwoordAanpassen));
+        }
+
+        public IActionResult NietDoorgaan(ProfielAanpassenViewModel model)
+        {
+            return RedirectToAction(nameof(ProfielAanpassen), model);
+        }
+
+        public IActionResult WachtwoordAanpassen()
+        {
+            return View(new WachtwoordAanpassenViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> WachtwoordAanpassen(WachtwoordAanpassenViewModel model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ApplicationUser user = await _userManager.GetUserAsync(User);
+                    bool check = await _userManager.CheckPasswordAsync(user, model.OldPassword);
+                    if (!check)
+                    {
+                        ModelState.AddModelError("OldPassword", "Incorrect wachtwoord");
+                    }
+                    else
+                    {
+                        var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                        var result = await _userManager.ResetPasswordAsync(user, token, model.Password);
+                        if (result.Succeeded)
+                        {
+                            JobCoach jc = _jobCoachRepository.GetByEmail(user.Email);
+
+                            jc.Wachtwoord = model.Password;
+                            jc.MoetWachtwoordVeranderen = false;
+                            _jobCoachRepository.SaveChanges();
+                            return RedirectToAction(nameof(ProfielAanpassen));
+                        }
+                        ModelState.AddModelError("", "Onze excuses, er is iets verkeerd gelopen.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                }
+            }
+            return View(new WachtwoordAanpassenViewModel());
+        }
+
+        private bool ControleerOfModelVerandertIs(ProfielAanpassenViewModel model)
+        {
+            return !model.Naam.Equals(model.Origineel.Naam) 
+                || !model.Voornaam.Equals(model.Origineel.Voornaam) 
+                || !model.NaamBedrijf.Equals(model.Origineel.NaamBedrijf)
+                || !model.Straat.Equals(model.Origineel.Straat)
+                || !model.Nummer.Equals(model.Origineel.Nummer)
+                || !model.Bus.Equals(model.Origineel.Bus)
+                || !model.Postcode.Equals(model.Origineel.Postcode)
+                || !model.Gemeente.Equals(model.Origineel.Gemeente);
+        }
     }
 }
---
+++ src/Aalstprojecten2-groep4DOTNET/Models/ViewModels/Home/WachtwoordAanpassenViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aalstprojecten2_groep4DOTNET.Models.ViewModels.Home
+{
+    public class WachtwoordAanpassenViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Oud wachtwoord")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nieuw wachtwoord")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Bevestig nieuw wachtwoord")]
+        public string ConfirmPassword { get; set; }
+    }
+}
