e5948b6a61106e3d82f1dbdfac1faf55b97051e3
niels <niels@NIELSPC>
2017-04-23 11:04:33 +0200

    alle inline css (die nodig was) verwijderd

---
+++ src/Aalstprojecten2-groep4DOTNET/wwwroot/js/HoverIconen.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    $("#homeIcoon").hover(function () {
+        $("#homePrent").attr("src", "/images/iconen/home_hover.png");
+    }, function () {
+        $("#homePrent").attr("src", "/images/iconen/home.png");
+    });
+
+    $("#nieuwIcoon").hover(function () {
+        $("#nieuwPrent").attr("src", "/images/iconen/nieuw_hover.png");
+    }, function () {
+        $("#nieuwPrent").attr("src", "/images/iconen/nieuw.png");
+    });
+
+    $("#archiefIcoon").hover(function () {
+        $("#archiefPrent").attr("src", "/images/iconen/archief_hover.png");
+    }, function () {
+        $("#archiefPrent").attr("src", "/images/iconen/archief.png");
+    });
+
+    $("#verwijderIcoon").hover(function () {
+        $("#verwijderPrent").attr("src", "/images/iconen/bin_hover.png");
+    }, function () {
+        $("#verwijderPrent").attr("src", "/images/iconen/bin.png");
+    });
+
+    $("#verstuurIcoon").hover(function () {
+        $("#verstuurPrent").attr("src", "/images/iconen/send_hover.png");
+    }, function () {
+        $("#verstuurPrent").attr("src", "/images/iconen/send.png");
+    });
+
+    $("#downloadIcoon").hover(function () {
+        $("#downloadPrent").attr("src", "/images/iconen/download_hover.png");
+    }, function () {
+        $("#downloadPrent").attr("src", "/images/iconen/download.png");
+    });
+
+    $("#archiveerIcoon").hover(function () {
+        $("#archiveerPrent").attr("src", "/images/iconen/archive_hover.png");
+    }, function () {
+        $("#archiveerPrent").attr("src", "/images/iconen/archive.png");
+    });
+
+    $("#potloodIcoon").hover(function () {
+        $("#potloodPrent").attr("src", "/images/iconen/edit_hover.png");
+    }, function () {
+        $("#potloodPrent").attr("src", "/images/iconen/edit.png");
+    });
+});
