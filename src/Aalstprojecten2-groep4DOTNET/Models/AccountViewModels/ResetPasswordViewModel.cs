28a77d20212209f359509c19633d8c6ebc84495d
niels <niels@NIELSPC>
2017-05-02 15:07:51 +0200

    eersteKeerLogin

--- src/Aalstprojecten2-groep4DOTNET/Models/AccountViewModels/ResetPasswordViewModel.cs
+++ src/Aalstprojecten2-groep4DOTNET/Models/AccountViewModels/ResetPasswordViewModel.cs
@@ -12,14 +12,15 @@ namespace Aalstprojecten2_groep4DOTNET.Models.AccountViewModels
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage ="Wachtwoord is verplicht")]
+        [Display(Name ="Wachtwoord")]
+        [StringLength(100,ErrorMessage = "Het wachtwoord moet minstens 6 en hoogstens 100 karakters lang zijn.", MinimumLength =6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Bevestig wachtwoord")]
+        [Compare("Password", ErrorMessage = "De twee ingegeven wachtwoorden zijn niet hetzelfde.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
