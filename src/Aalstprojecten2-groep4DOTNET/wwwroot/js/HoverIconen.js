f930f5f3832863a8c18fdd783c195069ccda1dff
niels <niels@NIELSPC>
2017-05-04 14:27:56 +0200

    huuuuuy

--- src/Aalstprojecten2-groep4DOTNET/wwwroot/js/HoverIconen.js
+++ src/Aalstprojecten2-groep4DOTNET/wwwroot/js/HoverIconen.js
@@ -1,51 +1,51 @@
 ﻿$(document).ready(function () {
     $(".homeIcoon").hover(function () {
-        $(".homePrent").attr("src", "/images/iconen/home_hover.png");
+        $(this).find(".homePrent").attr("src", "/images/iconen/home_hover.png");
     }, function () {
-        $(".homePrent").attr("src", "/images/iconen/home.png");
+        $(this).find(".homePrent").attr("src", "/images/iconen/home.png");
     });
 
     $(".nieuwIcoon").hover(function () {
-        $(".nieuwPrent").attr("src", "/images/iconen/nieuw_hover.png");
+        $(this).find(".nieuwPrent").attr("src", "/images/iconen/nieuw_hover.png");
     }, function () {
-        $(".nieuwPrent").attr("src", "/images/iconen/nieuw.png");
+        $(this).find(".nieuwPrent").attr("src", "/images/iconen/nieuw.png");
     });
 
 
     $(".archiefIcoon").hover(function () {
-        $(".archiefPrent").attr("src", "/images/iconen/archief_hover.png");
+        $(this).find(".archiefPrent").attr("src", "/images/iconen/archief_hover.png");
     }, function () {
-        $(".archiefPrent").attr("src", "/images/iconen/archief.png");
+        $(this).find(".archiefPrent").attr("src", "/images/iconen/archief.png");
     });
 
     $(".verwijderIcoon").hover(function () {
-        $(".verwijderPrent").attr("src", "/images/iconen/bin_hover.png");
+        $(this).find(".verwijderPrent").attr("src", "/images/iconen/bin_hover.png");
     }, function () {
-        $(".verwijderPrent").attr("src", "/images/iconen/bin.png");
+        $(this).find(".verwijderPrent").attr("src", "/images/iconen/bin.png");
     });
 
     $(".verstuurIcoon").hover(function () {
-        $(".verstuurPrent").attr("src", "/images/iconen/send_hover.png");
+        $(this).find(".verstuurPrent").attr("src", "/images/iconen/send_hover.png");
     }, function () {
-        $(".verstuurPrent").attr("src", "/images/iconen/send.png");
+        $(this).find(".verstuurPrent").attr("src", "/images/iconen/send.png");
     });
 
     $(".downloadIcoon").hover(function () {
-        $(".downloadPrent").attr("src", "/images/iconen/download_hover.png");
+        $(this).find(".downloadPrent").attr("src", "/images/iconen/download_hover.png");
     }, function () {
-        $(".downloadPrent").attr("src", "/images/iconen/download.png");
+        $(this).find(".downloadPrent").attr("src", "/images/iconen/download.png");
     });
 
     $(".archiveerIcoon").hover(function () {
-        $(".archiveerPrent").attr("src", "/images/iconen/archive_hover.png");
+        $(this).find(".archiveerPrent").attr("src", "/images/iconen/archive_hover.png");
     }, function () {
-        $(".archiveerPrent").attr("src", "/images/iconen/archive.png");
+        $(this).find(".archiveerPrent").attr("src", "/images/iconen/archive.png");
     });
 
     $(".potloodIcoon").hover(function () {
-        $(".potloodPrent").attr("src", "/images/iconen/edit_hover.png");
+        $(this).find(".potloodPrent").attr("src", "/images/iconen/edit_hover.png");
     }, function () {
-        $(".potloodPrent").attr("src", "/images/iconen/edit.png");
+        $(this).find(".potloodPrent").attr("src", "/images/iconen/edit.png");
     });
 
 
@@ -55,26 +55,26 @@
 
 
     $(".sendIcoon").hover(function () {
-        $(".sendPrent").attr("src", "/images/iconen/send_hover.png");
+        $(this).find(".sendPrent").attr("src", "/images/iconen/send_hover.png");
     }, function () {
-        $(".sendPrent").attr("src", "/images/iconen/send.png");
+        $(this).find(".sendPrent").attr("src", "/images/iconen/send.png");
     });
 
     $(".binIcoon").hover(function () {
-        $(".binPrent").attr("src", "/images/iconen/bin_hover.png");
+        $(this).find(".binPrent").attr("src", "/images/iconen/bin_hover.png");
     }, function () {
-        $(".binPrent").attr("src", "/images/iconen/bin.png");
+        $(this).find(".binPrent").attr("src", "/images/iconen/bin.png");
     });
 
     $(".downloadIcoon").hover(function () {
-        $(".downloadPrent").attr("src", "/images/iconen/download_hover.png");
+        $(this).find(".downloadPrent").attr("src", "/images/iconen/download_hover.png");
     }, function () {
-        $(".downloadPrent").attr("src", "/images/iconen/download.png");
+        $(this).find(".downloadPrent").attr("src", "/images/iconen/download.png");
     });
 
     $(".deArchiveerIcoon").hover(function () {
-        $(".deArchiveerPrent").attr("src", "/images/iconen/unarchive_hover.png");
+        $(this).find(".deArchiveerPrent").attr("src", "/images/iconen/unarchive_hover.png");
     }, function () {
-        $(".deArchiveerPrent").attr("src", "/images/iconen/unarchive.png");
+        $(this).find(".deArchiveerPrent").attr("src", "/images/iconen/unarchive.png");
     });
 });
