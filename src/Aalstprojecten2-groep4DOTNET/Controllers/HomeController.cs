d3e8e3bceb41f54b8134312c9b42e0e80fc8f190
dreeki <andreas.dewitte@hotmail.com>
2017-05-15 11:05:27 +0200

    kleine fix

--- src/Aalstprojecten2-groep4DOTNET/Controllers/HomeController.cs
+++ src/Aalstprojecten2-groep4DOTNET/Controllers/HomeController.cs
@@ -335,8 +335,8 @@ namespace Aalstprojecten2_groep4DOTNET.Controllers
                 InterneMailJobcoach mail = _interneMailJobcoachRepository.GetById(User.Identity.Name, id);
                 _interneMailJobcoachRepository.Delete(mail);
                 _interneMailJobcoachRepository.SaveChanges();
-                TempData["message"] = "De mail van " + mail.InterneMail.Afzender + " met onderwerp " +
-                                      mail.InterneMail.Onderwerp + " werd succesvol verwijdert";
+                TempData["message"] = "De mail van " + mail.InterneMail.Afzender.Voornaam + " " + mail.InterneMail.Afzender.Naam + " met onderwerp " +
+                                      mail.InterneMail.Onderwerp + " werd succesvol verwijderd";
                 
             }
             catch
