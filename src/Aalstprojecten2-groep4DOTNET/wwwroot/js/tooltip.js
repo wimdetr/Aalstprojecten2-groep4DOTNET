881bae77ce7bedeae2f616aae8005eabc7ab7cdb
dreeki <andreas.dewitte@hotmail.com>
2017-06-17 12:06:16 +0200

    fixed

--- src/Aalstprojecten2-groep4DOTNET/wwwroot/js/tooltip.js
+++ src/Aalstprojecten2-groep4DOTNET/wwwroot/js/tooltip.js
@@ -1,5 +1,7 @@
 $(document)
     .ready(function () {
+        var temp = window.location.href.split("/");
+        var pagina = temp[temp.length - 1];
         var tijd = 1000;
         if ($("#animatiesAanUit").text().trim() === "Animaties aanzetten") {
             tijd = 0;
@@ -15,9 +17,18 @@ $(document)
                     $(".tooltip:not(.geenTooltip)")
                         .each(function () {
                             if ($(this).data("volgorde") === volgordeWaarden[huidige]) {
-                                $(this).find(".tooltiptext").removeClass("verbergTooltip tooltipFadeOut").addClass("toonTooltip");
-                                if (tijd !== 0) {
-                                    $(this).find(".tooltiptext").addClass("tooltipFadeIn");
+                                if (pagina !== "Index") {
+                                    if (!$(this).hasClass("hoofdSchermTooltip")) {
+                                        $(this).find(".tooltiptext").removeClass("verbergTooltip tooltipFadeOut").addClass("toonTooltip");
+                                        if (tijd !== 0) {
+                                            $(this).find(".tooltiptext").addClass("tooltipFadeIn");
+                                        }
+                                    }
+                                } else {
+                                    $(this).find(".tooltiptext").removeClass("verbergTooltip tooltipFadeOut").addClass("toonTooltip");
+                                    if (tijd !== 0) {
+                                        $(this).find(".tooltiptext").addClass("tooltipFadeIn");
+                                    }
                                 }
                             }
                         });
@@ -60,7 +71,13 @@ $(document)
             $(".tooltip:not(.geenTooltip)")
                 .each(function() {
                     if (volgordeWaarden.indexOf($(this).data("volgorde")) === -1) {
-                        volgordeWaarden.push($(this).data("volgorde"));
+                        if (pagina !== "Index") {
+                            if (!$(this).hasClass("hoofdSchermTooltip")) {
+                                volgordeWaarden.push($(this).data("volgorde"));
+                            }
+                        } else {
+                            volgordeWaarden.push($(this).data("volgorde"));
+                        }
                     }
                 });
             volgordeWaarden = volgordeWaarden.sort((a, b) => a - b);
@@ -85,12 +102,21 @@ $(document)
 
         function toonTooltipsOpPositie() {
             $(".tooltip:not(.geenTooltip)").each(function () {
-                    if ($(this).data("volgorde") === volgordeWaarden[huidige]) {
+                if ($(this).data("volgorde") === volgordeWaarden[huidige]) {
+                    if (pagina !== "Index") {
+                        if (!$(this).hasClass("hoofdSchermTooltip")) {
+                            $(this).find(".tooltiptext").removeClass("verbergTooltip tooltipFadeOut").addClass("toonTooltip");
+                            if (tijd !== 0) {
+                                $(this).find(".tooltiptext").addClass("tooltipFadeIn");
+                            }
+                        }
+                    } else {
                         $(this).find(".tooltiptext").removeClass("verbergTooltip tooltipFadeOut").addClass("toonTooltip");
                         if (tijd !== 0) {
                             $(this).find(".tooltiptext").addClass("tooltipFadeIn");
                         }
                     }
-                });
+                }
+            });
         }
     });
